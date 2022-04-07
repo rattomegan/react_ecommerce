4f541d3316c2084f152a665db04e19ef09a5c3e3
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2022-04-07 16:47:44 -0400

    Add first slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,6 +6,7 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
+  position: relative;
 
 `
 const Arrow = styled.div`
@@ -13,15 +14,84 @@ const Arrow = styled.div`
   height: 50px;
   background-color: #fff7f7;
   border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${props => props.direction ==='left' && '10px'};
+  right: ${props => props.direction === 'right' && '10px'};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`;
 
-`
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+  padding: 50px;
+`;
+
+const Img = styled.img`
+  height: 74%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+  text-transform: uppercase;
+`;
+
+const Description = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background: transparent;
+  cursor: pointer;
+`;
+
 const Slider = () => {
   return (
     <Container>
-      <Arrow>
+      <Arrow direction='left'>
         <ArrowLeftIcon />
       </Arrow>
-      <Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Img src='https://images.unsplash.com/photo-1531875456634-3f5418280d20?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>Spring Sale</Title>
+            <Description>Plant Sale 20% off new arrivals!</Description>
+            <Button>Shop Now</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction='right'>
         <ArrowRightIcon />
       </Arrow>
     </Container>
